4fb5c556b786ab3538613d86078e0c0ea08f5365
AbdullahAhmed <131833346+AbdullahAhmed903@users.noreply.github.com>
2026-01-10 18:35:23 +0200

    Disable Redis client configuration
    Comment out Redis client configuration and event listeners.

--- config/redis.js
+++ config/redis.js
@@ -2,19 +2,16 @@ import { createClient } from "redis";
 import logger from "./logger.js";
 
 const redisClient = createClient({
-  url: process.env.REDIS_URL || "redis://127.0.0.1:6379",
-   socket: {
-        connectTimeout: 5000, // ⛔ prevents 504
-      },
+  // url: process.env.REDIS_URL || "redis://127.0.0.1:6379",
+  //  socket: {
+  //       connectTimeout: 5000, // ⛔ prevents 504
+  //     },
 });
 
-redisClient.on("connect", () =>  logger.info("✅ Connected to Redis successfully"));
-redisClient.on("error", (err) =>  logger.error("❌ Redis connection error:", err));
+// redisClient.on("connect", () =>  logger.info("✅ Connected to Redis successfully"));
+// redisClient.on("error", (err) =>  logger.error("❌ Redis connection error:", err));
 
-await redisClient.connect().catch((err) => {
-      logger.error("❌ Redis connection failed", err);
-      redisClient = null; // allow retry later
-    });
+
   
 
 export default redisClient;
