f62ad0166f18e99af94b9c33aec3441f71733005
AbdullahAhmed <131833346+AbdullahAhmed903@users.noreply.github.com>
2026-01-10 18:51:32 +0200

    Refactor DB connection to use singleton pattern
    Refactored database connection to use a singleton class.

--- app/DB/connection-db.js
+++ app/DB/connection-db.js
@@ -6,16 +6,37 @@ import dbConfig from "./db-config.js";
 mongoose.set('strictQuery', false); // optional, suppress warnings
 
 // Using a function to connect to the database
-const connectDB = async () => {
-  try {
-    await mongoose.connect('mongodb+srv://DoctorSystem:01090524452@cluster0.fjnrf7u.mongodb.net/DoctorSystem');
-    console.log("✅ Connected to the database!");
-  } catch (err) {
-    console.error("❌ Cannot connect to the database!", err);
-    process.exit(1); // exit with failure
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect(`${dbConfig.url}`);
+//     console.log("✅ Connected to the database!");
+//   } catch (err) {
+//     console.error("❌ Cannot connect to the database!", err);
+//     process.exit(1); // exit with failure
+//   }
+// };
+
+// export default connectDB;
+
+
+// Using a class to manage the connection
+// Singleton pattern to ensure single connection instance
+class connectiondb {
+  constructor() {
+    if (connectiondb.instance) return connectiondb.instance;
+    this.connect();
+    connectiondb.instance = this;
   }
-};
 
-export default connectDB;
+  async connect() {
+    try {
+      await mongoose.connect(`${dbConfig.url}`);
+      console.log("✅ Connected to the database!");
+    } catch (err) {
+      console.error("❌ Cannot connect to the database!", err);
+      process.exit(1); // exit with failure
+    }
+  }
+}
 
-
+export default connectiondb;
