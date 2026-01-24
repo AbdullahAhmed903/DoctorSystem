77ab9c218ba08f086897c914d8cd810b5bd5378f
AbdullahAhmed903 <abdullahahmed02000@gmail.com>
2026-01-24 18:43:48 +0200

    update swagger configration

--- app/swagger/swagger.js
+++ app/swagger/swagger.js
@@ -1,3 +1,4 @@
+import swaggerUi from "swagger-ui-express";
 import swaggerJSDoc from "swagger-jsdoc";
 
 const options = {
@@ -6,37 +7,43 @@ const options = {
     info: {
       title: "Doctor-System API",
       version: "1.0.0",
+      description: "Swagger documentation for Doctor-System",
     },
-  servers: [
-  {
-    url: "http://localhost:3000/api/v1",
-    description: "Local development",
-  },
-  {
-    url: "https://doctor-system-lemon.vercel.app/api/v1",
-    description: "Production",
-  },
-],
-  components: {
+    servers: [
+      {
+        url: "https://doctor-system-lemon.vercel.app/api/v1",
+        description: "Production",
+      },
+      {
+        url: "http://localhost:3000/api/v1",
+        description: "Local Development",
+      },
+    ],
+    components: {
       securitySchemes: {
-    BedoAuth: {
-      type: "apiKey",
-      in: "header",
-      name: "Authorization",
-      description: "Use format: Bedo_<JWT>",
-    },
-  },
+        BedoAuth: {
+          type: "apiKey",
+          in: "header",
+          name: "Authorization",
+          description: "Use format: Bedo_<JWT>",
+        },
+      },
     },
     security: [
       {
-        bearerAuth: [],
+        BedoAuth: [],
       },
     ],
-
-
   },
-  apis: ["./app/swagger/docs/*.swagger.js"],
-
+  apis: ["./app/swagger/docs/*.swagger.js"], // ملفات الـ swagger comments
 };
 
 export const swaggerSpec = swaggerJSDoc(options);
+
+export default function swaggerHandler(req, res) {
+  if (req.url.startsWith("/api-docs")) {
+    return swaggerUi.setup(swaggerSpec)(req, res);
+  } else {
+    res.status(404).send("Not Found");
+  }
+}
--- index.js
+++ index.js
@@ -12,50 +12,39 @@ import connectiondb from "./app/DB/connection-db.js";
 import swaggerUi from "swagger-ui-express";
 import { swaggerSpec } from "./app/swagger/swagger.js";
 
-// const numCPUs = os.cpus().length;
-// if(cluster.isPrimary){
-//   logger.info(`Primary process ${process.pid} is running`);
-//   console.log(process.pid);
-  
 
-//   // Fork workers
-//   for (let i = 0; i < numCPUs; i++) {
-//     cluster.fork();
-//   }
-//    cluster.on("exit", (worker) => {
-//     logger.error(`Worker ${worker.process.pid} died. Restarting...`);
-//     cluster.fork();
-//   });
-// }
+const corsConfig = {
+  origin: "*", 
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+};
 
-// else{
-const corsConfig={
-  origin:"*",
-  Credential:true,
-  methods:["GET","POST","PUT","DELETE"]
-}
-const app=express();
+const app = express();
 app.set('trust proxy', 1);
+
 app.use(cookieParser());
+
 app.use((req, res, next) => {
-  if(req.originalUrl=="/api/v1/appointment/webhook"){
-    next()
-  }
-  else{
+  if (req.originalUrl === "/api/v1/appointment/webhook") {
+    next();
+  } else {
     express.json()(req, res, next);
   }
-  
-})
+});
+
 app.use(express.urlencoded({ extended: true }));
-app.use(cors(corsConfig))
-new connectiondb()
+app.use(cors(corsConfig));
+
+new connectiondb();
+
 morgan.token('id', (req) => req.id);
 app.use(
   morgan(':id :method :url :status :response-time ms', { stream: logger.stream })
 );
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-v1routes(app)
+
+v1routes(app);
 
 
 
@@ -103,5 +92,4 @@ app.listen(CONFIG.PORT,()=>{
 });
 }
 
-// }
 export default app
